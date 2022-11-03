e6277548c88ba4097660ffc279ddcee86fdbc5fe
goju2k <goju2k@naver.com>
2022-11-03 23:58:42 +0900

    dsdsdsaaaaa

--- index.js
+++ index.js
@@ -5,17 +5,41 @@ const {BasicCredentialHandler} = require('@actions/http-client/lib/auth');
 const fs = require('fs');
 const path = require('path');
 
+
+//inputs
+const key = core.getInput('sonar.projectKey');
+const host = core.getInput('sonar.host.url');
+const login = core.getInput('sonar.login');
+const token = core.getInput('github.token')
+
+console.log(`inputs => key:${key} / host:${host} / login:${login}`);
+
+//http
+const authHandler = new BasicCredentialHandler(token, "")
+const http = new HttpClient('github-action', [authHandler])
+
+class Comment {
+  comment
+  constructor(comment){
+    this.comment = comment
+  }
+  pad(str){
+    this.comment += str
+  }
+  add(str){
+    this.pad('\n'+str)
+  }
+  toString(){
+    return this.comment
+  }
+}
+
 const run = async () => {
   
   try{
 
-    //inputs
-    const key = core.getInput('sonar.projectKey');
-    const host = core.getInput('sonar.host.url');
-    const login = core.getInput('sonar.login');
-    const token = core.getInput('github.token')
-
-    console.log(`inputs => key:${key} / host:${host} / login:${login}`);
+    //comment
+    let comment = new Comment('[SonarQube Check Result]')
     
     //get task file
     const taskFilePath = '.scannerwork/report-task.txt'
@@ -25,10 +49,27 @@ const run = async () => {
     const taskFileString = fs.readFileSync(path.resolve(homePath, taskFilePath), {encoding:'utf8'})
     console.log('taskFileString =>\n'+taskFileString);
 
-    const ceTaskUrl = taskFileString.split('ceTaskUrl=')[1]
+    //task status get
+    const ceTaskUrl = taskFileString.split('ceTaskUrl=')[1].replace(/\\r/g, '')
+    const taskInfo = await getTaskInfo(ceTaskUrl)
 
-    const taskStatus = await checkTaskStatus(ceTaskUrl, login)
+    comment.add(`Result : ${taskInfo.status}`)
+
+    //task detail get
+    const taskDetail = await getTaskDetail(taskInfo.componentKey)
+    comment.add('')
+    comment.add('[Summary]')
+    comment.add(`Check Total : ${taskDetail.paging.total}`)
+    comment.add(`Bugs  Total : ${taskDetail.baseComponent.measures[0].value}`)
+    comment.add('')
+    comment.add('[Details]')
+    taskDetail.components.forEach((elem)=>{
+      if(Number(elem.measures[0].value) > 0){
+        comment.add(`${elem.path} : ${taskDetail.paging.total}`)
+      }
+    })
     
+    //create pr comment
     const octo = github.getOctokit(token)
 
     octo.rest.issues.createComment({
@@ -36,7 +77,7 @@ const run = async () => {
       repo:github.context.issue.repo,
       issue_number:github.context.issue.number,
       title:'test issue from action',
-      body:`[SonarQube Check Result]\nCheck Status => ${taskStatus} !!!`
+      body:comment.toString()
     })
 
   }catch(error){
@@ -44,33 +85,49 @@ const run = async () => {
   }
 }
 
-const checkTaskStatus = async (ceTaskUrl, token) => {
+const getTaskDetail = async (componentKey) => {
+
+  const res = await http.get(`http://34.64.90.204:9000/api/measures/component_tree?component=${componentKey}&metricKeys=bugs`)
+  const bodyString = await res.readBody()
+  console.log(`[getTaskDetail][${bodyString}] received!!!`);
+  return JSON.parse(bodyString)
+  //console.log(`[getTaskDetail]\n${JSON.stringify(body, undefined, 2)}`);
+
+}
+
+/**
+ * Task Status Check
+ * @param {*} ceTaskUrl 
+ * @param {*} token 
+ * @returns 
+ */
+const getTaskInfo = async (ceTaskUrl) => {
 
   return new Promise((resolve)=>{
 
-    const authHandler = new BasicCredentialHandler(token, "")
-    const http = new HttpClient('github-action', [authHandler])
-  
     const startTime = new Date().getTime()
+    let count = 0
     const inter = setInterval(async ()=>{
   
       if(new Date().getTime() - startTime > 600000){ //10분 타임아웃
         clearInterval(inter)
-        throw new Error('[checkTaskStatus] 10 min timed out!!!')
+        throw new Error('[getTaskInfo] 10 min timed out!!!')
       }
 
-      console.log(`[${ceTaskUrl}] send!!!`);
+      count += 1
+
+      console.log(`[getTaskInfo][try ${count}][${ceTaskUrl}] send!!!`);
       const res = await http.get(ceTaskUrl)
       const bodyString = await res.readBody()
-      console.log(`[${bodyString}] received!!!`);
+      console.log(`[getTaskInfo][try ${count}][${bodyString}] received!!!`);
       const body = JSON.parse(bodyString)
-      console.log(`[${ceTaskUrl}]\n${JSON.stringify(body, undefined, 2)}`);
+      console.log(`[getTaskInfo][try ${count}][${ceTaskUrl}]\n${JSON.stringify(body, undefined, 2)}`);
 
       const status = body.task.status
 
       if(status != 'PENDING' && status != 'IN_PROGRESS'){
         clearInterval(inter)
-        resolve(status)
+        resolve(body.task)
       }
   
     }, 3000) //3초 마다 체크
