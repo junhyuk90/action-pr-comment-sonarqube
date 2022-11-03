adfe6fbfb337ad88ba451daef9020471b073fda5
goju2k <goju2k@naver.com>
2022-11-03 21:12:38 +0900

    update

--- dist/index.js
+++ dist/index.js
@@ -9686,41 +9686,85 @@ var __webpack_exports__ = {};
 (() => {
 const core = __nccwpck_require__(5590);
 const github = __nccwpck_require__(985);
-const http = __nccwpck_require__(4471);
+const {HttpClient} = __nccwpck_require__(4471);
+const {BearerCredentialHandler} = __nccwpck_require__(5213);
 const fs = __nccwpck_require__(7147);
 const path = __nccwpck_require__(1017);
 
-try{
-
-  //inputs
-  const key = core.getInput('sonar.projectKey');
-  const host = core.getInput('sonar.host.url');
-  const login = core.getInput('sonar.login');
-  const token = core.getInput('github.token')
-
-  console.log(`inputs => key:${key} / host:${host} / login:${login}`);
+const run = async () => {
   
-  //get task file
-  const taskFilePath = '.scannerwork/report-task.txt'
-  const homePath = `/home/runner/work/${github.context.issue.repo}/${github.context.issue.repo}`
-  console.log('home path => '+homePath);
-  console.log('find taskFile => '+taskFilePath);
-  const taskFileString = fs.readFileSync(path.resolve(homePath, taskFilePath), {encoding:'utf8'})
-  console.log('taskFileString =>\n'+taskFileString);
-  
-  const octo = github.getOctokit(token)
+  try{
 
-  octo.rest.issues.createComment({
-    owner:github.context.issue.owner,
-    repo:github.context.issue.repo,
-    issue_number:github.context.issue.number,
-    title:'test issue from action',
-    body:'hahahaha\n줄바꿈 되나??'
+    //inputs
+    const key = core.getInput('sonar.projectKey');
+    const host = core.getInput('sonar.host.url');
+    const login = core.getInput('sonar.login');
+    const token = core.getInput('github.token')
+
+    console.log(`inputs => key:${key} / host:${host} / login:${login}`);
+    
+    //get task file
+    const taskFilePath = '.scannerwork/report-task.txt'
+    const homePath = `/home/runner/work/${github.context.issue.repo}/${github.context.issue.repo}`
+    console.log('home path => '+homePath);
+    console.log('find taskFile => '+taskFilePath);
+    const taskFileString = fs.readFileSync(path.resolve(homePath, taskFilePath), {encoding:'utf8'})
+    console.log('taskFileString =>\n'+taskFileString);
+
+    const ceTaskUrl = taskFileString.split('ceTaskUrl=')[1]
+
+    const taskStatus = await checkTaskStatus(ceTaskUrl, login)
+    
+    const octo = github.getOctokit(token)
+
+    octo.rest.issues.createComment({
+      owner:github.context.issue.owner,
+      repo:github.context.issue.repo,
+      issue_number:github.context.issue.number,
+      title:'test issue from action',
+      body:`[SonarQube Check Result]\nCheck Status => ${taskStatus} !!!`
+    })
+
+  }catch(error){
+    core.setFailed(error.message);
+  }
+}
+
+const checkTaskStatus = async (ceTaskUrl, token) => {
+
+  return new Promise((resolve)=>{
+
+    const authHandler = new BearerCredentialHandler(token)
+    const http = new HttpClient('github-action', authHandler)
+  
+    const startTime = new Date().getTime()
+    const inter = setInterval(async ()=>{
+  
+      if(new Date().getTime() - startTime > 600000){ //10분 타임아웃
+        clearInterval(inter)
+        throw new Error('[checkTaskStatus] 10 min timed out!!!')
+      }
+
+      const res = await http.get(ceTaskUrl)
+      const body = JSON.parse(await res.readBody())
+      console.log(`[${ceTaskUrl}]\n${JSON.stringify(body, undefined, 2)}`);
+
+      const status = body.projectStatus.status
+
+      if(status != 'PENDING' && status != 'IN_PROGRESS'){
+        clearInterval(inter)
+        resolve(status)
+      }
+  
+    }, 3000) //3초 마다 체크
+
   })
 
-}catch(error){
-  core.setFailed(error.message);
 }
+
+//run task
+run()
+
 })();
 
 module.exports = __webpack_exports__;
--- index.js
+++ index.js
@@ -1,37 +1,80 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const http = require('@actions/http-client');
+const {HttpClient} = require('@actions/http-client');
+const {BearerCredentialHandler} = require('@actions/http-client/lib/auth');
 const fs = require('fs');
 const path = require('path');
 
-try{
-
-  //inputs
-  const key = core.getInput('sonar.projectKey');
-  const host = core.getInput('sonar.host.url');
-  const login = core.getInput('sonar.login');
-  const token = core.getInput('github.token')
-
-  console.log(`inputs => key:${key} / host:${host} / login:${login}`);
+const run = async () => {
   
-  //get task file
-  const taskFilePath = '.scannerwork/report-task.txt'
-  const homePath = `/home/runner/work/${github.context.issue.repo}/${github.context.issue.repo}`
-  console.log('home path => '+homePath);
-  console.log('find taskFile => '+taskFilePath);
-  const taskFileString = fs.readFileSync(path.resolve(homePath, taskFilePath), {encoding:'utf8'})
-  console.log('taskFileString =>\n'+taskFileString);
-  
-  const octo = github.getOctokit(token)
+  try{
 
-  octo.rest.issues.createComment({
-    owner:github.context.issue.owner,
-    repo:github.context.issue.repo,
-    issue_number:github.context.issue.number,
-    title:'test issue from action',
-    body:'hahahaha\n줄바꿈 되나??'
+    //inputs
+    const key = core.getInput('sonar.projectKey');
+    const host = core.getInput('sonar.host.url');
+    const login = core.getInput('sonar.login');
+    const token = core.getInput('github.token')
+
+    console.log(`inputs => key:${key} / host:${host} / login:${login}`);
+    
+    //get task file
+    const taskFilePath = '.scannerwork/report-task.txt'
+    const homePath = `/home/runner/work/${github.context.issue.repo}/${github.context.issue.repo}`
+    console.log('home path => '+homePath);
+    console.log('find taskFile => '+taskFilePath);
+    const taskFileString = fs.readFileSync(path.resolve(homePath, taskFilePath), {encoding:'utf8'})
+    console.log('taskFileString =>\n'+taskFileString);
+
+    const ceTaskUrl = taskFileString.split('ceTaskUrl=')[1]
+
+    const taskStatus = await checkTaskStatus(ceTaskUrl, login)
+    
+    const octo = github.getOctokit(token)
+
+    octo.rest.issues.createComment({
+      owner:github.context.issue.owner,
+      repo:github.context.issue.repo,
+      issue_number:github.context.issue.number,
+      title:'test issue from action',
+      body:`[SonarQube Check Result]\nCheck Status => ${taskStatus} !!!`
+    })
+
+  }catch(error){
+    core.setFailed(error.message);
+  }
+}
+
+const checkTaskStatus = async (ceTaskUrl, token) => {
+
+  return new Promise((resolve)=>{
+
+    const authHandler = new BearerCredentialHandler(token)
+    const http = new HttpClient('github-action', authHandler)
+  
+    const startTime = new Date().getTime()
+    const inter = setInterval(async ()=>{
+  
+      if(new Date().getTime() - startTime > 600000){ //10분 타임아웃
+        clearInterval(inter)
+        throw new Error('[checkTaskStatus] 10 min timed out!!!')
+      }
+
+      const res = await http.get(ceTaskUrl)
+      const body = JSON.parse(await res.readBody())
+      console.log(`[${ceTaskUrl}]\n${JSON.stringify(body, undefined, 2)}`);
+
+      const status = body.projectStatus.status
+
+      if(status != 'PENDING' && status != 'IN_PROGRESS'){
+        clearInterval(inter)
+        resolve(status)
+      }
+  
+    }, 3000) //3초 마다 체크
+
   })
 
-}catch(error){
-  core.setFailed(error.message);
 }
+
+//run task
+run()
