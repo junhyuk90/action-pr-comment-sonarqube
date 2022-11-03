66c29b036575fe44bb64187ceda7bf3ca2e8617a
goju2k <goju2k@naver.com>
2022-11-03 18:07:57 +0900

    token added

--- dist/index.js
+++ dist/index.js
@@ -9692,10 +9692,18 @@ try{
   const key = core.getInput('sonar.projectKey');
   const host = core.getInput('sonar.host.url');
   const login = core.getInput('sonar.login');
-  const secrets = core.getInput('secrets')
-  console.log('secrets', JSON.stringify(secrets, undefined, 2));
+  const token = core.getInput('github.token')
+  console.log(`inputs => key:${key} / host:${host} / login:${login}`);
+  console.log('secrets', token);
   
-  //github.getOctokit()
+  const octo = github.getOctokit(token)
+
+  octo.rest.issues.create({
+    owner:'goju',
+    repo:'ui-play',
+    title:'test issue from action',
+    body:'hahahaha\n줄바꿈 되나??'
+  })
 
 }catch(error){
   core.setFailed(error.message);
